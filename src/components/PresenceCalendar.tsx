5d47dfec30d8d3761afe09cdfeaad5c0c944e316
Thomas <thomas.biarneix@gmail.com>
2025-01-17 12:29:17 +0100

    feat(presence calendar): update ui

--- src/components/PresenceCalendar.tsx
+++ src/components/PresenceCalendar.tsx
@@ -131,10 +131,10 @@ export function PresenceCalendar({
                 s.activeChildren.includes(currentUserId)
               )?.id || null
             )}
-            className="p-2 hover:bg-gray-400 rounded-full bg-black flex items-center gap-2"
+            className="inline-flex items-center px-3 py-2 gap-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50                     "
           >
-            <Pencil className="w-4 h-4 text-gray-100" />
-            <span className="text-gray-100 text-sm">
+            <Pencil className="w-4 h-4 text-gray-700" />
+            <span className="text-gray-700 text-sm">
               {editingSubgroupId ? "Terminer l'édition" : "Éditer mes présences"}
             </span>
           </button>
